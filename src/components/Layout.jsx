9779cd2d130d48f72520c9b9ebcb561e46eeaa1a
Cyril Le Goas <cyril.legoas@gmail.com>
2022-11-10 00:10:20 +0100

    CHANGE background color as prop

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Header } from './Header';
 import { Footer } from './Footer';
 
-export function Layout({ children }) {
+export function Layout({ children, bgColor = 'light' }) {
   return (
     <>
       <Header />
-      <main className="flex-1">{children}</main>
+      <main
+        className={`flex-1 ${bgColor === 'dark' ? 'bg-dark' : 'bg-current'}`}
+      >
+        {children}
+      </main>
       <Footer />
     </>
   );
