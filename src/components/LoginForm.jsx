d07627ba13db29e9a22022325b19554642477258
Cyril Le Goas <cyril.legoas@gmail.com>
2022-11-18 16:48:49 +0100

    ADD user login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
+import { useForm } from '../hooks/useForm';
+import { login } from '../actions';
 import { UserIcon } from './UserIcon';
 
 export function LoginForm() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [values, handleChange] = useForm({ email: '', password: '' });
+  const { fetchCall, data, error } = useFetch(
+    'http://localhost:3001/api/v1/user/login'
+  );
+
+  useEffect(() => {
+    if (!data) return;
+    const jwt = data.body.token;
+    dispatch(login(jwt));
+    navigate('/profile');
+  }, [data, dispatch, navigate]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    navigate('/profile');
+    fetchCall({
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: values.email,
+        password: values.password,
+      }),
+    });
   };
 
   return (
@@ -19,14 +44,19 @@ export function LoginForm() {
         <UserIcon />
         <h2 className="font-medium text-[1.5rem] my-[0.2rem]">Sign In</h2>
         <div>
-          <label htmlFor="username" className="font-medium">
-            Username
+          <label htmlFor="email" className="font-medium">
+            Email
           </label>
           <input
             type="text"
-            id="username"
+            id="email"
+            value={values.email}
+            onChange={handleChange}
             className="w-full p-[5px] text-[1.2rem] border-[1px] border-grey rounded"
           />
+          {error === 'Error: User not found!' ? (
+            <p className="text-red text-xs py-1">{error}</p>
+          ) : null}
         </div>
         <div>
           <label htmlFor="password" className="font-medium">
@@ -35,8 +65,13 @@ export function LoginForm() {
           <input
             type="password"
             id="password"
+            value={values.password}
+            onChange={handleChange}
             className="w-full p-[5px] text-[1.2rem] border-[1px] border-grey rounded"
           />
+          {error === 'Error: Password is invalid' ? (
+            <p className="text-red text-xs py-1">{error}</p>
+          ) : null}
         </div>
         <div className="w-full flex">
           <input type="checkbox" id="remember-me" className="mx-1" />
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,22 @@
+import { useCallback, useState } from 'react';
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchCall = useCallback(
+    async (options) => {
+      try {
+        const res = await fetch(url, options);
+        const json = await res.json();
+        if (json.status >= 400) setError(json.message);
+        else setData(json);
+      } catch (errorMessage) {
+        setError(new Error(errorMessage));
+      }
+    },
+    [url]
+  );
+
+  return { fetchCall, data, error };
+};
