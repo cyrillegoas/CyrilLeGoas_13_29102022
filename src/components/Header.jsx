0c9d05f6746ab4d14f194901f85e9ea2da4daf04
Cyril Le Goas <cyril.legoas@gmail.com>
2022-11-21 17:26:16 +0100

    ADD logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { UserIcon } from './UserIcon';
+import { logout } from '../actions';
 
 export function Header() {
+  const { firstName } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   return (
-    <header className="flex items-center justify-between px-5 py-[0.3125rem] w-screen">
+    <header className="flex items-center justify-between px-5 py-[0.3125rem]">
       <Link to="/">
         <img
           src="/logos/argentBankLogo.png"
@@ -13,11 +17,30 @@ export function Header() {
         />
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
-      <nav className="mr-2">
-        <Link to="/login" className="flex items-center gap-2 font-bold">
-          <UserIcon />
-          Sign In
-        </Link>
+      <nav className="mr-2 flex gap-2">
+        {firstName ? (
+          <>
+            <Link to="/profile" className="flex items-center gap-2 font-bold">
+              <UserIcon />
+              {firstName}
+            </Link>
+            <Link
+              to="/"
+              className="flex items-center gap-2 font-bold"
+              onClick={() => {
+                dispatch(logout());
+              }}
+            >
+              <img src="/icons/icon-logout.svg" alt="" className="w-[16px]" />
+              Sign Out
+            </Link>
+          </>
+        ) : (
+          <Link to="/login" className="flex items-center gap-2 font-bold">
+            <UserIcon />
+            Sign In
+          </Link>
+        )}
       </nav>
     </header>
   );
